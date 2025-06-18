db3745b4d486af403a29ff0597a25e27743e1bba
mrnithesh <mr.nithesh.k@gmail.com>
2025-06-18 17:41:02 +0530

    implemented binary search function

--- Solutions/Week 1/Data structures and Algorithms/E-commerce Platform Search/src/App.java
+++ Solutions/Week 1/Data structures and Algorithms/E-commerce Platform Search/src/App.java
@@ -20,16 +20,43 @@ public class App {
         return null;
     }
 
+    public static Product BinarySearch(Product[] products, int target){
+        int left = 0;
+        int right = products.length-1;
+        while (left<=right){
+            int mid = (left+right) / 2;
+            if (products[mid].productID == target){
+                return products[mid];
+            }
+            else if (products[mid].productID < target){
+                left = mid+1;
+            }
+            else{
+                right = mid -1;
+            }
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
         Product[] products = {new Product(11,"Apple Watch","Electronics"),
         new Product(12, "Ooty Apple", "Fruits"),
         new Product(19, "KitKat", "Chocoloates")
         };
 
-        Product pr = LinearSearch(products,12);
-        if (pr!= null){
+        Product pr1 = LinearSearch(products,12);
+        if (pr1!= null){
             System.out.println("Product found!");
-            System.out.println("Product id :"+ pr.productID+"\nProdcut Name: "+ pr.productName +"\nProduct Category:"+ pr.category);
+            System.out.println("Product id :"+ pr1.productID+"\nProdcut Name: "+ pr1.productName +"\nProduct Category:"+ pr1.category);
+        }
+        else{
+            System.out.println("Product not found");
+        }
+
+        Product pr2 = BinarySearch(products,19);
+        if (pr2!= null){
+            System.out.println("Product found!");
+            System.out.println("Product id :"+ pr2.productID+"\nProdcut Name: "+ pr2.productName +"\nProduct Category:"+ pr2.category);
         }
         else{
             System.out.println("Product not found");
