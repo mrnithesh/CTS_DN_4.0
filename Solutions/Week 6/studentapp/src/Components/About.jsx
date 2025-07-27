01ce0f099bc64a3a79a2cfb6b9621a0e0ea7728b
mrnithesh <mr.nithesh.k@gmail.com>
2025-07-27 17:57:02 +0530

    adding week 6 react HOL 2

---
+++ Solutions/Week 6/studentapp/src/Components/About.jsx
@@ -0,0 +1,8 @@
+import React, {Component} from "react";
+class About extends Component{
+    render(){
+        return <h2>Welcome to the About Page of the Student Management Portal</h2>;
+    }
+}
+
+export default About;
---
+++ Solutions/Week 6/studentapp/src/Components/Contact.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Contact extends React.Component {
+  render() {
+    return <h2>Welcome to the Contact Page of the Student Management Portal</h2>;
+  }
+}
+
+export default Contact;
